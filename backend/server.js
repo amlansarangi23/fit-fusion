f8918b89f90306096fb075d6a39826c8d382a99f
Amlan Sarangi <117941624+amlansarangi23@users.noreply.github.com>
2024-06-18 20:01:40 +0530

    add cors

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,22 @@
 require('dotenv').config()
 
+
 const express = require('express')
 const mongoose = require('mongoose')
 const workoutRoutes = require('./routes/workouts')
 const userRoutes = require('./routes/user')
+const cors = require("cors");
 
 // express app
 const app = express()
 
 // middleware
+const corsOptions = {
+  origin: "http://localhost:3000"
+}
+
 app.use(express.json())
+app.use(cors(corsOptions));
 
 app.use((req, res, next) => {
   console.log(req.path, req.method)
