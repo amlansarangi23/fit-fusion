5f42164d6049788ab96cd1c9b647a7f6581dd2f2
Amlan Sarangi <117941624+amlansarangi23@users.noreply.github.com>
2024-06-24 17:27:48 +0530

    check

--- frontend/src/pages/About.js
+++ frontend/src/pages/About.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import Hero from "../aboutcomponents/Hero";
-import Analytics from "../aboutcomponents/Analytics";
-import Newsletter from "../aboutcomponents/Newsletter";
-import Cards from "../aboutcomponents/Cards";
+// import Analytics from "../aboutcomponents/Analytics";
+// import Newsletter from "../aboutcomponents/Newsletter";
+// import Cards from "../aboutcomponents/Cards";
 
 const About = () => {
     return (
-        <div className="bg-gray-950">
+        <div>
         <Hero />
-        <Analytics />
+        {/* <Analytics />
         <Newsletter />
-        <Cards />
+        <Cards /> */}
         </div>
 
     );
