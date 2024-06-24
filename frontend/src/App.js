6cc3525541d5337739ba67275493f4e364cd7063
Amlan Sarangi <117941624+amlansarangi23@users.noreply.github.com>
2024-06-24 19:47:08 +0530

    fix refresh error

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,39 +1,43 @@
-import { BrowserRouter, Routes, Route, Navigate} from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { useAuthContext } from './hooks/useAuthContext';
 
 // pages & components
-import Home from './pages/Home'
-import Navbar from './components/Navbar'
+import Home from './pages/Home';
+import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 import About from './pages/About';
 
-
-
 function App() {
-  const { user } = useAuthContext()
+  const { user } = useAuthContext();
+
   return (
     <div className="App">
       <BrowserRouter>
         <Navbar />
         <div className="pages w-[100vw]">
           <Routes>
-            <Route 
-              path="/" 
-              element={user ? <Home /> : <Navigate to="/about" />} 
+            <Route
+              path="/"
+              element={user ? <Home /> : <Navigate to="/about" />}
             />
-            <Route 
-              path="/login" 
-              element={!user ? <Login /> : <Navigate to="/" />} 
+            <Route
+              path="/login"
+              element={!user ? <Login /> : <Navigate to="/" />}
             />
-            <Route 
-              path="/signup" 
-              element={!user ? <Signup /> : <Navigate to="/" />} 
+            <Route
+              path="/signup"
+              element={!user ? <Signup /> : <Navigate to="/" />}
             />
-            <Route 
-              path="/about" 
-              element={<About />} 
+            <Route
+              path="/about"
+              element={<About />}
+            />
+            {/* Catch-all route for 404 Not Found */}
+            <Route
+              path="*"
+              element={<Navigate to="/" />}
             />
           </Routes>
         </div>
