26e1a3f1b382b7785ae7ef1b27542786ba61d2a3
Amlan Sarangi <117941624+amlansarangi23@users.noreply.github.com>
2024-06-24 17:29:56 +0530

    check2

--- frontend/src/pages/About.js
+++ frontend/src/pages/About.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import Hero from "../aboutcomponents/Hero";
-// import Analytics from "../aboutcomponents/Analytics";
-// import Newsletter from "../aboutcomponents/Newsletter";
+import Analytics from "../aboutcomponents/Analytics";
+import Newsletter from "../aboutcomponents/Newsletter";
 // import Cards from "../aboutcomponents/Cards";
 
 const About = () => {
     return (
         <div>
         <Hero />
-        {/* <Analytics />
+        <Analytics />
         <Newsletter />
-        <Cards /> */}
+        {/* <Cards /> */}
         </div>
 
     );
