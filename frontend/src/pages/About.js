252c8c3add5c1893cfbdc50e625dada64d332177
Amlan Sarangi <117941624+amlansarangi23@users.noreply.github.com>
2024-06-24 17:24:59 +0530

    fix

--- frontend/src/pages/About.js
+++ frontend/src/pages/About.js
@@ -1,18 +1,18 @@
 import React from 'react';
-// import Hero from "../aboutcomponents/Hero";
-// import Analytics from "../aboutcomponents/Analytics";
-// import Newsletter from "../aboutcomponents/Newsletter";
-// import Cards from "../aboutcomponents/Cards";
+import Hero from "../aboutcomponents/Hero";
+import Analytics from "../aboutcomponents/Analytics";
+import Newsletter from "../aboutcomponents/Newsletter";
+import Cards from "../aboutcomponents/Cards";
 
 const About = () => {
     return (
-    //     <div className="bg-gray-950">
-    //     <Hero />
-    //     <Analytics />
-    //     <Newsletter />
-    //     <Cards />
-    //   </div>
-    <h1>hello</h1>
+        <div className="bg-gray-950">
+        <Hero />
+        <Analytics />
+        <Newsletter />
+        <Cards />
+        </div>
+
     );
 };
 
